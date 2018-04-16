32f9dea2f440d21923698963fbeea0da8a514780
ViduraDa <vidurada@gmail.com>
2018-04-16 21:37:00 +0530

    complete

--- PAalgorithm.py
+++ PAalgorithm.py
@@ -1,20 +1,6 @@
 import numpy as np
 import csv
 
-# Classic Method
-def classic(lt, xt):
-    return lt / xt
-
-
-# First Relaxation Method
-def first(C, lt, xt):
-    return min(C, (lt / xt))
-
-
-# Second Relaxation Method
-def second(C, lt, xt):
-    return lt / (xt + (1 / (2 * C)))
-
 
 # input
 train_data = np.loadtxt("train_data.csv", dtype='i', delimiter=',')
@@ -24,7 +10,7 @@ test_labels = np.loadtxt("test_labels.csv", dtype='i', delimiter=',')
 
 iterations = 10
 C = 1
-update_option = 'classic'
+update_option = 'second'
 
 # PA algoritm
 # initiate
@@ -38,27 +24,29 @@ for i in range(iterations):
     for index in range(len(train_data)):
         # receive the x instance at time t
         xt = train_data[index, :]
-        #print(xt)
+        #print(np.dot(w, xt))
         # predict
         y = train_labels[index] * np.dot(w, xt)
-        #print(ys)
+        #print(y)
         if y < 1:
             # loss calculation
-            lt = max(0, (1 - y))
+            loss = max(0, (1 - y))
             xt_square = np.sum(xt ** 2)
 
             # select the updating option
             if update_option == 'classic':
-                tt = classic(lt, xt_square)
+                tt = loss / xt_square
             elif update_option == 'first':
-                tt = first(C, lt, xt_square)
+                tt = min(C, (loss / xt_square))
             else:
-                tt = second(C, lt, xt_square)
+                tt = loss / (xt_square + (1 / (2 * C)))
 
             # update weights
             w = w + tt * train_labels[index] * xt
     print("w = ", w)
     print("")
+
+
     # testing
     for r in range(len(test_data)):
         # receive the x instance at time t
